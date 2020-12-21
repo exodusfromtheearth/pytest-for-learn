621f0b33a4170ebf1b6d43432113c65253b5a0c2
gxt <15611360995@163.com>
2020-12-21 09:54:16 +0800

    test

--- base/operationExcel.py
+++ base/operationExcel.py
@@ -44,12 +44,12 @@ class OperationExcel(OperationYaml):
 
     def getexceldatas(self):
         '''第一步获取所有测试用例'''
-        datas = list()
+        runlist = list()
         title = self.getsheet().row_values(0)
         for row in range(1, self.getrows()):
             rowvalue = self.getsheet().row_values(row)
-            datas.append(dict(zip(title, rowvalue)))
-        return datas
+            runlist.append(dict(zip(title, rowvalue)))
+        return runlist
 
     def caserunwithoutpara(self):
         '''第二步获取可执行的测试用例'''
@@ -66,36 +66,32 @@ class OperationExcel(OperationYaml):
         '''第三步关联yaml文件，获取测试用例的参数data和期望结果expect'''
         runlist = list()
         for item in self.caserunwithoutpara():
-            rowvalue = self.getrows()-1
-            for row in range(0, rowvalue):
-                item[ExcelVariables.expect] = self.readyaml(self.fileroot, self.yamlname)[row]["expect"]
-                item[ExcelVariables.data] = self.readyaml(self.fileroot, self.yamlname)[row]["para"]
+            num = item[ExcelVariables.expect]
+            item[ExcelVariables.data] = self.readyaml(self.fileroot, self.yamlname)[int(num)]["para"]
+            item[ExcelVariables.expect] = self.readyaml(self.fileroot, self.yamlname)[int(num)]["expect"]
             runlist.append(item)
         return runlist
 
-    # 将接口返回值写入Excel
-    def write_case_depend(self, content, row):
+    # 将返回的状态码和断言结果写入Excel
+    def write_actual_return(self, content1, content2, row):
         rb = self.book
         wb = copy(rb)
         w_sheet = wb.get_sheet(self.num)
-        w_sheet.write(row, TestCaseKeyWord.case_depend, content)
-        wb.save(filepath(self.fileroot, self.excelname))
-
-    # 将接口返回的状态码写入Excel
-    def write_actual_status_code(self, content, row):
-        rb = self.book
-        wb = copy(rb)
-        w_sheet = wb.get_sheet(self.num)
-        w_sheet.write(row, TestCaseKeyWord.actual_status_code, content)
+        w_sheet.write(row, TestCaseKeyWord.actual_status_code, content1)
+        w_sheet.write(row, TestCaseKeyWord.result, content2)
         wb.save(filepath(self.fileroot, self.excelname))
 
 
 if __name__ == '__main__':
-    obj = OperationExcel(2, 'data/Arrow', 'arrow_test.xlsx', 'fundinglist.yaml')
+    obj = OperationExcel(2, 'data/Arrow', 'arrow_test3.xlsx', 'fundinglist.yaml')
     # for item in obj.getexceldatas():
     #     print(item[ExcelVariables.is_execute])
     # print(obj.getsheet().cell_value(1, 12))
     # for i in range(0, obj.getcols()):
     #     print(obj.getsheet().cell_value(1, i))
     # print(obj.caserunwithoutpara())
+    # for i in obj.caserun():
+    #     print(i["请求参数"],i["期望结果"])
     # print(obj.caserun())
+    # obj.write_case_depend("断言成功", 1)
+    obj.write_actual_return("200", "断言成功", 1)
--- case/test_funding_new.py
+++ case/test_funding_new.py
@@ -4,31 +4,41 @@
 # Time:2020/9/6 4:31 下午
 
 from base.method import Requests
+from base.excelKeyWord import TestCaseKeyWord
 from base.operationExcel import OperationExcel, ExcelVariables
 import pytest
 import json
 from config import readConfig
 
 
-excel = OperationExcel(1, 'data/Arrow', 'arrow_test.xlsx', 'fundinglist.yaml')
+excel = OperationExcel(2, 'data/Arrow', 'arrow_test.xlsx', 'fundinglist.yaml')
 obj = Requests()
 
 
+# caserun已经过滤不执行的测试用例
 @pytest.mark.parametrize('datas', excel.caserun())
 def test_funding_list(datas):
     url = readConfig.host+datas[ExcelVariables.url]
     params = datas[ExcelVariables.data]
+    print(type(params))
     method = datas[ExcelVariables.method]
     content_type = datas[ExcelVariables.header]
+    # print(content_type)
     header = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36",
-        "Accept": "application/json",
+        'Accept': "application/json",
         # "Content-Type": content_type,
+        # "Content-Type": "application/json",
         "Cookie": readConfig.cookie
     }
     expect = datas[ExcelVariables.expect]
     r = obj.request(url=url, headers=header, data=params, method=method)
-    assert expect in json.dumps(r.json(), ensure_ascii=False)
+    # print(r.json()["code"])
+    assert expect in json.dumps(r.json(), ensure_ascii=False), "断言失败"
+    # try:
+    #     assert expect in json.dumps(r.json(), ensure_ascii=False)
+    # except AssertionError as e:
+    #     print(e)
 
 
 if __name__ == '__main__':
